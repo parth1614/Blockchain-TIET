0840f202042ede139e3f4983d013e04ef8e4aebe
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-08-29 07:03:38 +0000

    StyledComponents in Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/index';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar/>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {Nav,NavbarContainer,MobileIcon,NavLogo,NavMenu,NavItem,NavLinks} from './NavbarElements';
+import {FaBars} from 'react-icons/fa'
 
 const Navbar = () => {
   return(
     <>
   <Nav>
   <NavbarContainer>
-  <NavLogo>
+  <NavLogo to="/">
   DappTIET
   </NavLogo>
+  <MobileIcon>
+  <FaBars />
+  </MobileIcon>
+  <NavMenu>
+  <NavItem>
+  <NavLinks to="about">About</NavLinks>
+  </NavItem>
+  </NavMenu>
   </NavbarContainer>
   </Nav>
   </>
